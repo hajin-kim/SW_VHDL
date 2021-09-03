26c672156997feb50895b7c9471ff61f266dfd12
Hajin_Kim <kimhajin@yonsei.ac.kr>
2021-09-03 19:11:12 +0900

    fixed mem cell async problem

--- sources_1/mem_cell.vhd
+++ sources_1/mem_cell.vhd
@@ -17,21 +17,23 @@ entity mem_cell is
   ) ;
 end entity ; -- mem_cell
 
-architecture mem_cell_async of mem_cell is
+architecture mem_cell_arch of mem_cell is
 	signal data: std_logic_vector(MEM_DATA_WIDTH-1 downto 0) ;
 begin
 
-	cell : process( areset_n, w_en, w_data )
+	cell : process( clock )
 	begin
-		if ( areset_n = '0' ) then
-			data <= std_logic_vector(to_unsigned(0, MEM_DATA_WIDTH));
-		else
-			if ( w_en = '1' ) then
-				data <= w_data;
+		if ( rising_edge(clock) ) then
+			if ( areset_n = '0' ) then
+				data <= std_logic_vector(to_unsigned(0, MEM_DATA_WIDTH));
+			else
+				if ( w_en = '1' ) then
+					data <= w_data;
+				end if ;
 			end if ;
 		end if ;
 	end process ; -- cell
 
 	Qout <= data;
 
-end architecture ; -- mem_cell_async
+end architecture ; -- mem_cell_arch
